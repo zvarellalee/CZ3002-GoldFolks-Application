1efbcb8c5d099886bdd9cea4c3649abe4183d17f
zvarellalee <62020585+zvarellalee@users.noreply.github.com>
2021-09-30 18:56:16 +0800

    Auto rotate video player

--- lib/view/VideoPlayerScreen.dart
+++ lib/view/VideoPlayerScreen.dart
@@ -1,35 +1,66 @@
 import 'package:flutter/material.dart';
-import 'package:youtube_player_flutter/youtube_player_flutter.dart';
+import 'package:flutter/services.dart';
+import 'package:youtube_player_iframe/youtube_player_iframe.dart';
 
-class VideoPlayerScreen extends StatelessWidget {
+class VideoPlayerScreen extends StatefulWidget {
   static String id = 'VideoPlayerScreen';
   @override
+  _VideoPlayerScreenState createState() => _VideoPlayerScreenState();
+}
+
+class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    //SystemChrome.setPreferredOrientations([DeviceOrientation.landscapeLeft]);
+  }
+
+
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.landscapeLeft,
+    ]);
     Map args = ModalRoute.of(context).settings.arguments;
     print(args);
     YoutubePlayerController _controller = YoutubePlayerController(
       initialVideoId: args['videoId'],
-      flags: YoutubePlayerFlags(
+      params: YoutubePlayerParams(
         autoPlay: true,
         mute: false,
+        desktopMode: true,
+        privacyEnhanced: true,
+        //showControls: false,
       ),
     );
-    return Scaffold(
-      body: Container(
-        color: Colors.black,
-        child: YoutubePlayerBuilder(
-          player: YoutubePlayer(
+    return WillPopScope(
+      onWillPop: () async {
+        SystemChrome.setPreferredOrientations([
+          DeviceOrientation.portraitUp,
+        ]);
+        return true;
+      },
+      child: Scaffold(
+        body: Container(
+          color: Colors.black,
+          alignment: Alignment.center,
+          child: YoutubePlayerControllerProvider( // Provides controller to all the widget below it.
             controller: _controller,
-          ),
-            builder: (context, player) {
-              return Column(
-                children: [
-                  player,
-                ],
-              );
-            },
+            child: YoutubePlayerIFrame(
+              aspectRatio: 16 / 9,
+            ),
           ),
         ),
-      );
+      )
+    );
   }
 }
+
+
